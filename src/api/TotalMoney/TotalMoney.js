f8dd0b145069528be3330f88286c8092fe01b8b6
Shahidul-Islam-Jony <shahidulislam13sf@gmail.com>
2024-05-29 16:46:14 +0600

    Own money get success

--- src/api/TotalMoney/TotalMoney.js
+++ src/api/TotalMoney/TotalMoney.js
@@ -3,28 +3,38 @@ const AddCost = require("../../models/AddCost/AddCost");
 
 const TotalMoney = async (req, res) => {
   try {
-    const users = await Money.find();
-    //   console.log(users);
-    let totalMoney = 0;
-    let totalCost = 0;
-    users.forEach((user) => {
-      user?.money.forEach((amount) => {
-        totalMoney += amount;
+    const uId = req.params?.uid;
+    // console.log(uId);
+    if (uId !== "all") {      // find own money details
+      const query = {uid:uId};
+      const ownMoney = await Money.find(query);
+      // console.log(ownMoney);
+      res.send(ownMoney);
+
+    } else {          // get all money details
+      const users = await Money.find();
+      console.log(users);
+      let totalMoney = 0;
+      let totalCost = 0;
+      users.forEach((user) => {
+        user?.money.forEach((amount) => {
+          totalMoney += amount;
+        });
       });
-    });
 
-    //   console.log(totalMoney);
+      //   console.log(totalMoney);
 
-    const benifitedUsers = await AddCost.find();
-    // console.log(benifitedUsers);
-    benifitedUsers.forEach((user) => {
-      totalCost += user?.money;
-    });
-    // console.log(totalCost);
+      const benifitedUsers = await AddCost.find();
+      // console.log(benifitedUsers);
+      benifitedUsers.forEach((user) => {
+        totalCost += user?.money;
+      });
+      // console.log(totalCost);
 
-    totalMoney -= totalCost;
+      totalMoney -= totalCost;
 
-    res.send({ totalMoney, totalCost });
+      res.send({ totalMoney, totalCost });
+    }
   } catch (err) {
     res.status(500).json({ message: "Internal server error" });
   }
--- src/routes/TotalMoney/totalMoneyRoute.js
+++ src/routes/TotalMoney/totalMoneyRoute.js
@@ -2,6 +2,6 @@ const express = require("express");
 const TotalMoney = require("../../api/TotalMoney/TotalMoney");
 const router = express.Router();
 
-router.get("/total/money", TotalMoney); //getting total money and total cost
+router.get("/total/money/:uid", TotalMoney); //getting total money and total cost && getting own money details
 
 module.exports = router;
