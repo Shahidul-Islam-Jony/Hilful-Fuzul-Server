9a1c76f8f005c12055b75ab40d65b5246e558a82
Shahidul-Islam-Jony <shahidulislam13sf@gmail.com>
2024-06-09 22:20:51 +0600

    update photo url and user type combine in one api

--- src/api/UpdateUserType/UpdateUserType.js
+++ src/api/UpdateUserType/UpdateUserType.js
@@ -1,13 +1,22 @@
 const Users = require("../../models/Users/Users");
 
 const UpdateUserType = async(req,res)=>{
-    const {type,uid} = req.body;
-    // console.log(type,uid);
-    const query = {uid:uid};
-    const updateType = {type:type};
-    const result = await Users.updateOne(query,updateType);
-    // console.log(result);
-    res.send(result);
+    const data = req.body;
+    // console.log(data?.imageUrl);
+    const query = {uid:data?.uid};
+    if(data?.type){
+        const updateType = {type:data?.type};
+        const result = await Users.updateOne(query,updateType);
+        // console.log(result);
+        res.send(result);
+    }
+    else{
+        const updateType = {photoUrl:data?.imageUrl};
+        const result = await Users.updateOne(query,updateType);
+        // console.log(result);
+        res.send(result);
+    }
+
 }
 
 module.exports = UpdateUserType;
